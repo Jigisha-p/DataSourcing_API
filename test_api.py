b33d292a23a28dcc6d0e7747e338411d50da7b95
Jigisha P <65441412+Jigisha-p@users.noreply.github.com>
2023-07-17 08:28:42 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://api.openweathermap.org/geo/1.0/direct?q=Barcelona&appid=xxxxxxxxxxxx"
+url = "https://api.openweathermap.org/geo/1.0/direct?q=Barcelona&appid=xxxxxxxxxxxx"  # replace your api id value for the parameter appid
 response = requests.get(url).json()
 city = response[0]
 print(f"{city['name']}: ({city['lat']}, {city['lon']})")
